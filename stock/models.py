80d6634006113b68005ce3e929e02b2f067de5e4
EricksonLopes <ofc.erickson@gmail.com>
2020-09-07 12:34:54 -0300

    Meta abstract

--- stock/models.py
+++ stock/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 
-class Product(models.Model):
-    name = models.Charfield(mex_lenght=255)
+# Para as dias classes se comportarem da mesma forma / atribuir data de atualizção e criação
+class TimestambleMixin(models.Model):
+    # Vai pegar data hora do momento
+    created_at = models.DateTimeField(auto_now_add=True)
+    # toda vez que for atualizado
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Product(TimestambleMixin):
+    name = models.CharField(max_length=255)
     stock = models.IntegerField(default=0)
     stock_max = models.IntegerField()
     price_sale = models.DecimalField(decimal_places=2, max_digits=6)
     price_purchase = models.DecimalField(decimal_places=2, max_digits=6)
+
+
+class StockEntry(TimestambleMixin):
+    amount = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
